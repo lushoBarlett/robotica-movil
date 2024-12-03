a51e4c82f63198476abeab2fb340bd46b6f91c20
Luciano Barletta <lushobarlett@gmail.com>
2024-12-03 12:35:37 -0300

    preciosos gráficos normalizados

--- tp4/tests.py
+++ tp4/tests.py
@@ -2,7 +2,15 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-r = [1/64, 1/16, 1/4, 4, 16, 64]
+
+R = [1/64, 1/16, 1/4, 4, 16, 64]
+
+
+COLORS = ["red", "blue", "green", "orange", "purple", "brown"]
+
+
+PARTICLES = [20, 50, 500]
+
 
 def test_ekf():
     print("Running EKF tests")
@@ -10,12 +18,13 @@ def test_ekf():
     with open("ekf.txt", "w") as f:
         f.write("")
 
-    for i in r:
+    for i in R:
         print(f"Running 10 runs for r = {i}")
         for _ in range(10):
             command = f"python localization.py ekf --data-factor {i} --filter-factor {i} >> ekf.txt"
             print("$", command)
             os.system(f"python localization.py ekf --data-factor {i} --filter-factor {i} >> ekf.txt")
+
 
 def test_pf():
     print("Running PF tests")
@@ -23,28 +32,28 @@ def test_pf():
     with open("pf.txt", "w") as f:
         f.write("")
 
-    for i in r:
+    for i in R:
         print(f"Running 10 runs for r = {i}")
         for _ in range(10):
             command = f"python localization.py pf --data-factor {i} --filter-factor {i} >> pf.txt"
             print("$", command)
             os.system(f"python localization.py pf --data-factor {i} --filter-factor {i} >> pf.txt")
 
+
 def test_pf_extensive():
     print("Running PF extensive tests")
 
-    particles = [20, 50, 500]
-
-    for n in particles:
+    for n in PARTICLES:
         with open(f"pfext{n}.txt", "w") as f:
             f.write("")
 
-        for i in r:
+        for i in R:
             print(f"Running 10 runs for r = {i} and n = {n}")
             for _ in range(10):
                 command = f"python localization.py pf --data-factor {i} --filter-factor {i} --num-particles {n} >> pfext{n}.txt"
                 print("$", command)
                 os.system(f"python localization.py pf --data-factor {i} --filter-factor {i} --num-particles {n} >> pfext{n}.txt")
+
 
 """
 Example output of one run:
@@ -66,6 +75,7 @@ class Run:
     def __repr__(self):
         return f"Run(data_factor={self.data_factor}, filter_factor={self.filter_factor}, mean_error={self.mean_error}, mean_mahalanobis={self.mean_mahalanobis}, anees={self.anees})"
 
+
 def parse_run(lines: list[str]):
 
     if (len(lines) < 6):
@@ -78,14 +88,46 @@ def parse_run(lines: list[str]):
     anees = float(lines[5].split(":")[-1].strip())
     return Run(data_factor, filter_factor, mean_error, mean_mahalanobis, anees), lines[6:]
 
-"""
-Graficar el error de posición medio a medida que los factores α, β varían sobre r y discuta los
-resultados.
 
-Graficar el error de posición medio y ANEES a medida que los factores α, β del filtro varían sobre
-r mientras los datos son generados con los valores por defecto.
-"""
-def plot(runs: list[Run], prefix: str = "", log=False):
+def style_normalized(x, ys, names):
+    normalized = [(y - np.min(y)) / (np.max(y) - np.min(y)) for y in ys]
+
+    for i, (y, n, p, c) in enumerate(zip(ys, normalized, names, COLORS)):
+        plt.plot(x, n, label=p, color=c)
+
+        plt.xticks(x, map(lambda x: f"1 / {int(1 / x)}" if 0 < x < 1 else "0" if x == 0 else str(int(x)), x))
+
+        min_index = np.argmin(n)
+        max_index = np.argmax(n)
+
+        plt.scatter(x[min_index], 0, color=c, zorder=5)  # Min point
+        plt.scatter(x[max_index], 1, color=c, zorder=5)  # Max point
+
+        plt.annotate(
+            f"Min: {y[min_index]:.2f}",
+            (x[min_index], 0),
+            textcoords="offset points",
+            xytext=(-40, 5 + i * 10),
+            ha='center',
+            color=c
+        )
+
+        plt.annotate(
+            f"Max: {y[max_index]:.2f}",
+            (x[max_index], 1),
+            textcoords="offset points",
+            xytext=(40, -15 - i * 10),
+            ha='center',
+            color=c
+        )
+
+    plt.gca().axes.yaxis.set_visible(False)
+    plt.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.7)
+    plt.tight_layout()
+    plt.legend()
+
+
+def plot_data_from_runs(runs: list[Run]) -> tuple[list[float], list[float], list[float]]:
     x = sorted(set(run.data_factor for run in runs))
     y = []; y2 = []
     for r in x:
@@ -101,83 +143,37 @@ def plot(runs: list[Run], prefix: str = "", log=False):
 
         y2.append(avg_anees)
 
+    return x, y, y2
+
+
+"""
+Graficar el error de posición medio a medida que los factores α, β varían sobre r y discuta los
+resultados.
+
+Graficar el error de posición medio y ANEES a medida que los factores α, β del filtro varían sobre
+r mientras los datos son generados con los valores por defecto.
+"""
+def plot(runs: list[Run], prefix: str = "", log=False, title=None):
+    x, y, y2 = plot_data_from_runs(runs)
+
     plt.figure(figsize=(10, 6), dpi=120)
-    plt.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.7)
-
-    if log:
-        plt.xscale("log")
-    else:
-        plt.xscale("linear")
-
-    normalized = [i / (max(y) - min(y)) for i in y]
-
-    fig, ax1 = plt.subplots()
-    ax1.plot(x, y)
-    ax2 = ax1.twinx()
-    ax2.plot(x, normalized, color="red")
-
-    plt.plot(x, y)
-    plt.xlabel("r")
-    plt.ylabel("Mean position error")
+    plt.title(title)
+    plt.xscale("log" if log else "linear")
+    plt.xlabel("r" + (" (log scale)" if log else ""))
+    style_normalized(x, [y], ["Mean position error (normalized)"])
     plt.savefig(prefix + "mean_position_error.png")
     plt.show()
 
     plt.figure(figsize=(10, 6), dpi=120)
-    plt.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.7)
-
-    if log:
-        plt.xscale("log")
-    else:
-        plt.xscale("linear")
-
-    plt.plot(x, y, label="Mean position error")
-    plt.plot(x, y2, label="ANEES")
-    plt.xlabel("r")
-    plt.legend()
+    plt.title(title)
+    plt.xscale("log" if log else "linear")
+    plt.xlabel("r" + (" (log scale)" if log else ""))
+    style_normalized(x, [y, y2], ["Mean position error (normalized)", "ANEES (normalized)"])
     plt.savefig(prefix + "mean_position_error_and_anees.png")
     plt.show()
 
-def plot_normalized(x, ys):
-    normalized = [(y - np.min(y)) / (np.max(y) - np.min(y)) for y in ys]
 
-    colors = plt.cm.tab10(np.linspace(0, 1, len(ys)))
-
-    plt.figure(figsize=(10, 6))
-    for i, n in enumerate(normalized):
-        color = colors[i]
-
-        plt.plot(x, n, label=f'Line {i + 1}', color=color)
-
-        min_index = np.argmin(n)
-        max_index = np.argmax(n)
-
-        plt.scatter(x[min_index], 0, color=color, zorder=5)  # Min point
-        plt.scatter(x[max_index], 1, color=color, zorder=5)  # Max point
-
-        plt.annotate(
-            f"Min: {ys[i][min_index]:.2f}",
-            (x[min_index], 0),
-            textcoords="offset points",
-            xytext=(-40, 5),
-            ha='center',
-            color=color
-        )
-
-        plt.annotate(
-            f"Max: {ys[i][max_index]:.2f}",
-            (x[max_index], 1),
-            textcoords="offset points",
-            xytext=(40, -15),
-            ha='center',
-            color=color
-        )
-
-    plt.gca().axes.yaxis.set_visible(False)
-    plt.grid(color='gray', linestyle='--', alpha=0.7)
-    plt.tight_layout()
-    plt.show()
-
-def plot_runs_from_file(filename: str, log=False):
+def read_run(filename: str):
     with open(filename, "r") as f:
         lines = f.readlines()
 
@@ -187,7 +183,8 @@ def plot_runs_from_file(filename: str, log=False):
             if run:
                 runs.append(run)
 
-        plot(runs, filename.split(".")[0] + "_", log)
+        return runs
+
 
 if __name__ == "__main__":
     # used in the first run because it takes a long time
@@ -195,8 +192,23 @@ if __name__ == "__main__":
     # test_pf()
     # test_pf_extensive()
 
-    plot_runs_from_file("ekf.txt", log=True)
-    plot_runs_from_file("pf.txt", log=True)
+    plot(read_run("ekf.txt"), "ekf_", log=True, title="Extended Kalman Filter")
+    plot(read_run("pf.txt"), "pf_", log=True, title="Particle Filter")
 
-    for n in [20, 50, 500]:
-        plot_runs_from_file(f"pfext{n}.txt", log=True)
+    plt.figure(figsize=(10, 6), dpi=120)
+    plt.title("Particle Filter with different number of particles")
+    plt.xscale("log")
+    plt.xlabel("r (log scale)")
+
+    ys = []
+    names = []
+    for n in PARTICLES:
+        xaxis, y, y2 = plot_data_from_runs(read_run(f"pfext{n}.txt"))
+        ys.append(y)
+        ys.append(y2)
+        names.append(f"Mean position error (normalized) for {n} particles")
+        names.append(f"ANEES (normalized) for {n} particles")
+
+    style_normalized(xaxis, ys, names)
+    plt.savefig("pf_mean_position_error_and_anees_many_particles.png")
+    plt.show()
